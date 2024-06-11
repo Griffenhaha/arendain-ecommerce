ed3704fa2e35150b930251513b8a5f534eae627a
Griffenhaha <cyronjakearendain3@gmail.com>
2024-06-11 19:50:14 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,114 @@
+function order(){
+    var visible = document.getElementById("order");
+    visible.style.display="block";
+}
+
+function back(){
+    var close = document.getElementById("tsItem");
+    close.style.display="none";
+    var item = document.getElementById("items");
+    item.style.display="block";
+    var close = document.getElementById("shItem");
+    close.style.display="none";
+    var close = document.getElementById("shoesItem");
+    close.style.display="none";
+    var close = document.getElementById("bagItem");
+    close.style.display="none";
+    var close = document.getElementById("capItem");
+    close.style.display="none";
+    var close = document.getElementById("pantsItem");
+    close.style.display="none";
+}
+
+function it1(){
+    var close = document.getElementById("tsItem");
+    close.style.display="block";
+    var item = document.getElementById("items");
+    item.style.display="none";
+}
+
+function it2(){
+    var close = document.getElementById("shItem");
+    close.style.display="block";
+    var item = document.getElementById("items");
+    item.style.display="none";
+}
+function it3(){
+    var close = document.getElementById("shoesItem");
+    close.style.display="block";
+    var item = document.getElementById("items");
+    item.style.display="none";
+}
+function it4(){
+    var close = document.getElementById("bagItem");
+    close.style.display="block";
+    var item = document.getElementById("items");
+    item.style.display="none";
+}
+function it5(){
+    var close = document.getElementById("capItem");
+    close.style.display="block";
+    var item = document.getElementById("items");
+    item.style.display="none";
+}
+function it6(){
+    var close = document.getElementById("pantsItem");
+    close.style.display="block";
+    var item = document.getElementById("items");
+    item.style.display="none";
+}
+
+function label1(){
+    var label = document.getElementById("label1");
+    label.style.opacity="1";
+}
+
+function out1(){
+    var label = document.getElementById("label1");
+    label.style.opacity="0";
+}
+function label2(){
+    var label = document.getElementById("label2");
+    label.style.opacity="1";
+}
+
+function out2(){
+    var label = document.getElementById("label2");
+    label.style.opacity="0";
+}
+function label3(){
+    var label = document.getElementById("label3");
+    label.style.opacity="1";
+}
+
+function out3(){
+    var label = document.getElementById("label3");
+    label.style.opacity="0";
+}
+function label4(){
+    var label = document.getElementById("label4");
+    label.style.opacity="1";
+}
+
+function out4(){
+    var label = document.getElementById("label4");
+    label.style.opacity="0";
+}
+function label5(){
+    var label = document.getElementById("label5");
+    label.style.opacity="1";
+}
+
+function out5(){
+    var label = document.getElementById("label5");
+    label.style.opacity="0";
+}
+function label6(){
+    var label = document.getElementById("label6");
+    label.style.opacity="1";
+}
+
+function out6(){
+    var label = document.getElementById("label6");
+    label.style.opacity="0";
+}
